13ee4dddf8817b84a75b6c5e3be170aa0aaaa501
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2022-07-01 17:52:10 +0800

    feat: 調整 demo-link 樣式

--- windi.config.js
+++ windi.config.js
@@ -1,6 +1,6 @@
 export default {
   shortcuts: {
     'demo-link':
-      'block w-70 h-10 px-4 py-2 bg-[#cbd5e1] text-center rounded hover:(bg-[#94A3B8] transition-all)'
+      'block w-80 h-10 px-4 py-2 bg-[#cbd5e1] text-center rounded hover:(bg-[#94A3B8] transition-all)'
   }
 }
